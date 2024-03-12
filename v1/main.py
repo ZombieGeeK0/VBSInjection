adaf02ed005c53e59cedba6a0b13bc6aa804ee0b
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-12 11:25:25 +0100

    Update main.py

--- v1/main.py
+++ v1/main.py
@@ -1,1 +1,20 @@
 # importamos las librerías necesarias
+import os
+from colorama import Fore, Back
+
+# hacemos la clase color
+class color
+  RED = Fore.RED + Back.RESET
+  RESET = Fore.RESET + Back.RESET
+
+def main():
+  os.system('cls')
+  title = '''
+ ▌ ▐·▄▄▄▄· .▄▄ · ▪   ▐ ▄  ▐▄▄▄▄▄▄ . ▄▄· ▄▄▄▄▄▪         ▐ ▄ 
+▪█·█▌▐█ ▀█▪▐█ ▀. ██ •█▌▐█  ·██▀▄.▀·▐█ ▌▪•██  ██ ▪     •█▌▐█
+▐█▐█•▐█▀▀█▄▄▀▀▀█▄▐█·▐█▐▐▌▪▄ ██▐▀▀▪▄██ ▄▄ ▐█.▪▐█· ▄█▀▄ ▐█▐▐▌
+ ███ ██▄▪▐█▐█▄▪▐█▐█▌██▐█▌▐▌▐█▌▐█▄▄▌▐███▌ ▐█▌·▐█▌▐█▌.▐▌██▐█▌
+. ▀  ·▀▀▀▀  ▀▀▀▀ ▀▀▀▀▀ █▪ ▀▀▀• ▀▀▀ ·▀▀▀  ▀▀▀ ▀▀▀ ▀█▄▀▪▀▀ █▪
+'''
+
+# elite font
